26a49207953dd9c48499021b497cde76d58cc4d8
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-02-04 09:27:29 -0300

    UPDATE - Frontend/Component: auth - Main panel created
    added material angular:
    	- To create a visual design patter.
    	- Make easy the creation of components.

--- leendbook-frontend/src/app/auth/auth.module.ts
+++ leendbook-frontend/src/app/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthComponent } from './component/auth.component';
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent],
   providers: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
