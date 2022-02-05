16be4ecb10ab806a92ac6d7a72c8ab2c7fc102f2
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-02-04 21:20:51 -0300

    UPDATE/WORK-IN-PROGRESS - Data bind

--- leendbook-frontend/src/app/auth/component/auth.component.ts
+++ leendbook-frontend/src/app/auth/component/auth.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  _user: User = new User();
 
-  constructor(
-    private authService: AuthService
-  ) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.sayHello();
+
+    this._user.setFirstName('Rodrigo');
+  }
+
+  getUser(): User {
+    return this._user;
+  }
+
+  onChangeFirstName(event: any): void {
+    this.getUser().setFirstName(event.target.value);
+  }
+
+  onChangeLastName(event: any): void {
+    this.getUser().setLastName(event.target.value);
+  }
+
+  onChangeEmail(event: any): void {
+    this.getUser().setEmail(event.target.value);
+  }
+
+  onChangePassword(event: any): void {
+    this.getUser().setPassword(event.target.value);
+  }
+
+  onClickFinish(event: any): void {
+    console.log(this.getUser());
   }
 }
