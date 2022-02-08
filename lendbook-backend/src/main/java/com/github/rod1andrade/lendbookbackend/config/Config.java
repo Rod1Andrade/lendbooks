d8089daf9924b54b667dd739e9805b957061a7b6
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-02-07 21:46:10 -0300

    DEPLOY - Backend

--- lendbook-backend/src/main/java/com/github/rod1andrade/lendbookbackend/config/Config.java
+++ lendbook-backend/src/main/java/com/github/rod1andrade/lendbookbackend/config/Config.java
@@ -1,7 +1,14 @@
 package com.github.rod1andrade.lendbookbackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class Config {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
 }
